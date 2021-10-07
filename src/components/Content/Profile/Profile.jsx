b4b7356de73b380cf1db730269ea6fabb63abe28
treig <sources2005@yandex.ru>
2021-10-07 17:54:30 +0300

    improved Profile page

--- src/components/Content/Profile/Profile.jsx
+++ src/components/Content/Profile/Profile.jsx
@@ -1,8 +1,20 @@
-//import style from './Profile.module.css';
+import { createRef } from 'react';
+import style from './Profile.module.css';
 
-const Profile = () => {
+const Profile = (props) => {
+  const textareaPost = createRef();
   return (
-    <div className={`content`}>Содержание профиля</div>
+    <div className={`content ${style.content} ${style.profile}`}>
+      <div className={style.userInfo}><span>user info</span></div>
+      <div className={style.userPosts}>
+        <div className={style.postForm}>
+          <h1>Новая запись</h1>
+          <textarea ref={textareaPost}></textarea>
+          <input type="button" value="Добавить" />
+        </div>
+        <div></div>
+      </div>
+    </div>
   );
 };
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -6,4 +6,11 @@ const state = {
   }
 };
 
+export const addPost = (message) => {
+  const newPost = {
+    id: state.profileData.posts.length + 1, message, likesCount: 0
+  };
+  state.profileData.posts.push(newPost);
+};
+
 export default state;
