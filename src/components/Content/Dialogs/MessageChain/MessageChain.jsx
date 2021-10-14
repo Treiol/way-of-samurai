5f75b4aec102ae1892e9bcb35bb298cd476c29de
treig <sources2005@yandex.ru>
2021-10-14 16:31:36 +0300

    released message sending

--- src/components/Content/Dialogs/MessageChain/MessageChain.jsx
+++ src/components/Content/Dialogs/MessageChain/MessageChain.jsx
@@ -1,8 +1,13 @@
-//import style from './MessageChain.module.css';
+import style from './MessageChain.module.css';
 
 const MessageChain = (props) => {
+  const items = props.items.map(
+    (item, i) => <span key={`item${i + 1}`} className={style.item}>{item}</span>
+  );
+  const messageChainClassName =
+    (props.incoming) ? `${style.messageChain} ${style.incoming}` : style.messageChain;
   return (
-    <div></div>
+    <div className={messageChainClassName}>{items}</div>
   );
 };
 
