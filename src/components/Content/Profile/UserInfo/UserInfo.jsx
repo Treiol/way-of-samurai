2aabeb6198210bcd4e04cf5c6e54ca0669b2b910
treig <sources2005@yandex.ru>
2022-01-18 18:16:06 +0300

    updated UserInfo

--- src/components/Content/Profile/UserInfo/UserInfo.jsx
+++ src/components/Content/Profile/UserInfo/UserInfo.jsx
@@ -1,21 +1,38 @@
+import React from 'react';
 import style from './UserInfo.module.css';
 
-const UserInfo = (props) => {
-  const userInfo = [];
-  if (!props.userInfo) {
-    userInfo.push(<span key="userInfo">user info</span>);
-  } else {
-    userInfo.push(<div key="userAvatar" className={style.userAvatar}><span>ava</span></div>);
-    userInfo.push(
-      <div key="userData" className={style.userData}>
-        <span className={style.name}>{props.userInfo.name}</span>
-        <span className={style.aboutMe}>{props.userInfo.about_me}</span>
-      </div>
-    );
+class UserInfo extends React.Component {
+  // ---------------------------------------------------
+  state = { editMode: false }
+  // ---------------------------------------------------
+  setEditMode = (value) => {
+    console.log(this);
+    this.setState({ editMode: value });
   }
-  return (
-    <div className={style.userInfo}>{userInfo}</div>
-  );
+  // ---------------------------------------------------
+  render() {
+    const userInfo = [];
+    if (!this.props.userInfo) {
+      userInfo.push(<span key="userInfo">user info</span>);
+    } else {
+      const aboutMe = (this.state.editMode)
+        ? <input
+            id="aboutMe" type="text" placeholder="Обо мне" value={this.props.userInfo.about_me}
+            autoFocus={true}
+            onBlur={() => { this.setEditMode(false); }}
+          />
+        : this.props.userInfo.about_me;
+      userInfo.push(<div key="userAvatar" className={style.userAvatar}><span>ava</span></div>);
+      userInfo.push(
+        <div key="userData" className={style.userData}>
+          <span className={style.name}>{this.props.userInfo.name}</span>
+          <span className={style.aboutMe} onClick={() => { this.setEditMode(true); }}>{aboutMe}</span>
+        </div>
+      );
+    }
+    return (<div className={style.userInfo}>{userInfo}</div>);
+  }
+  // ---------------------------------------------------
 };
 
 export default UserInfo;
